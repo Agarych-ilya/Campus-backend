af7a73867ba3246bdfbb0170ac0a2fa3ae5454ce
treskunchik <lukmot211@gmail.com>
2026-06-30 14:59:37 +0300

    Shedule: schedule parsing logic has been implemented

--- bd/registration/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# База данных
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_NAME = os.path.join(BASE_DIR, 'registration.db')
-
-# Настройки bcrypt (cost factor). 12 — хороший баланс скорости/безопасности
-BCRYPT_ROUNDS = 12
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'; // <--- ДОЛЖНО БЫТЬ СТРОГО НА ПЕРВОЙ СТРОЧКЕ ФАЙЛА
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
